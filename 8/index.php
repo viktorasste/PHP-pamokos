6c6684855acfb136c15f532b481f2ff2fab7cc07
flawas1 <105674788+viktorasste@users.noreply.github.com>
2022-09-28 20:08:32 +0300

    9
    9

---
+++ 8/index.php
@@ -0,0 +1,14 @@
+<?php
+
+//Masyvu funkcijos//
+
+//unset() - funkcija, kuri sunaikina masyvo elementa.
+
+$masyvas = array(1,2,3,4,5)
+
+
+//dvimatis masyvas//(lentele)
+
+//array_keys() - funkcija kuri grazina masyvo indeksus(masyvo lemento numeris)
+//array_values() - funkcija kuri grazina masyvo reiksmes.
+
